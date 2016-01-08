f3a556088f5adccbd2d518b58941c00a3168b408
Liwen Wen <wenliwen64@gmail.com>
2016-01-08 10:36:28 -0800

    finish plot.C

--- plot_scripts/plot.C
+++ plot_scripts/plot.C
@@ -20,6 +20,7 @@
 	hangle_diff_west_ep->Scale(1./hangle_diff_west_ep->GetEntries());
 	Name.Form("EP orientation diff. with multiplicity %d", n_par);
 	hangle_diff_west_ep->SetTitle(Name.Data());
+	hangle_diff_west_ep->GetXaxis()->SetTitle("Angle Diff.");
 	hangle_diff_west_ep->Draw();
 
         // Input
@@ -30,12 +31,102 @@
     }
     can->SaveAs("angle_diff_50to600.eps");
     can->SaveAs("angle_diff_50to600.png");
+
 // 1. MSC 
     TCanvas* can_msc = new TCanvas("can_msc", "can_msc", 1600, 900);
     can_msc->Divide(4, 3, small, small);
 
     for(Int_t i = 0; i < 12; i++){
-        f[i]->Get("msc_os_v2_west_ep");
+        can_msc->cd(i+1);
+	Int_t n_par = (i+1) * 50;
+        TProfile* msc_os_rp = (TProfile*)f[i]->Get("msc_os_v2_rp");
+        TProfile* msc_ss_rp = (TProfile*)f[i]->Get("msc_ss_v2_rp");
+
+        msc_os_rp->SetMarkerStyle(25); // empty-square
+	msc_ss_rp->SetMarkerStyle(25);
+	msc_os_rp->SetLineColor(4);
+	msc_os_rp->SetMarkerColor(4);
+	msc_ss_rp->SetLineColor(2);
+	msc_ss_rp->SetMarkerColor(2);
+
+        TString title;
+	title.Form("msc input v.s. recon. with n_part_%d", n_par);
+	msc_os_rp->SetTitle(title.Data());
+	msc_os_rp->GetYaxis()->SetRangeUser(-0.003, 0.003);
+	msc_os_rp->Draw("p");
+	msc_ss_rp->Draw("p same");
+
+        TProfile* msc_os_west_ep = (TProfile*)f[i]->Get("msc_os_v2_west_ep");
+        TProfile* msc_ss_west_ep = (TProfile*)f[i]->Get("msc_ss_v2_west_ep");
+
+        msc_os_west_ep->SetMarkerStyle(8);
+        msc_ss_west_ep->SetMarkerStyle(8);
+	msc_os_west_ep->SetLineColor(4);
+	msc_os_west_ep->SetMarkerColor(4);
+	msc_ss_west_ep->SetLineColor(2);
+	msc_ss_west_ep->SetMarkerColor(2);
+
+        msc_os_west_ep->Draw("p same");
+        msc_ss_west_ep->Draw("p same");
+
+	TLegend* leg = new TLegend(.4, .14, .65, .32);
+	leg->SetBorderSize(0);
+	leg->AddEntry(msc_os_rp, "os_real_rp");
+	leg->AddEntry(msc_ss_rp, "ss_real_rp");
+	leg->AddEntry(msc_os_west_ep, "os_recon_ep");
+	leg->AddEntry(msc_ss_west_ep, "ss_recon_ep");
+	leg->Draw();
     }
+    can_msc->SaveAs("msc_50to60.eps");
+    can_msc->SaveAs("msc_50to60.png");
+
 // 2. Gamma
+    TCanvas* can_gamma = new TCanvas("can_gamma", "can_gamma", 1600, 900);
+    can_gamma->Divide(4, 3, small, small);
+
+    for(Int_t i = 0; i < 12; i++){
+        can_gamma->cd(i+1);
+	Int_t n_par = (i+1) * 50;
+        TProfile* gamma_os_rp = (TProfile*)f[i]->Get("gamma_os_v2_rp");
+        TProfile* gamma_ss_rp = (TProfile*)f[i]->Get("gamma_ss_v2_rp");
+
+        gamma_os_rp->SetMarkerStyle(25); // empty-square
+	gamma_ss_rp->SetMarkerStyle(25);
+	gamma_os_rp->SetLineColor(4);
+	gamma_os_rp->SetMarkerColor(4);
+	gamma_ss_rp->SetLineColor(2);
+	gamma_ss_rp->SetMarkerColor(2);
+
+        TString title;
+	title.Form("gamma input v.s. recon. with n_part_%d", n_par);
+	gamma_os_rp->SetTitle(title.Data());
+	gamma_os_rp->GetYaxis()->SetRangeUser(-0.003, 0.003);
+	gamma_os_rp->Draw("p");
+	gamma_ss_rp->Draw("p same");
+
+        TProfile* gamma_os_west_ep = (TProfile*)f[i]->Get("gamma_os_v2_west_ep");
+        TProfile* gamma_ss_west_ep = (TProfile*)f[i]->Get("gamma_ss_v2_west_ep");
+
+        gamma_os_west_ep->SetMarkerStyle(8);
+        gamma_ss_west_ep->SetMarkerStyle(8);
+	gamma_os_west_ep->SetLineColor(4);
+	gamma_os_west_ep->SetMarkerColor(4);
+	gamma_ss_west_ep->SetLineColor(2);
+	gamma_ss_west_ep->SetMarkerColor(2);
+
+        gamma_os_west_ep->Draw("p same");
+        gamma_ss_west_ep->Draw("p same");
+
+	TLegend* leg = new TLegend(.4, .14, .65, .32);
+	leg->SetBorderSize(0);
+	leg->AddEntry(gamma_os_rp, "os_real_rp");
+	leg->AddEntry(gamma_ss_rp, "ss_real_rp");
+	leg->AddEntry(gamma_os_west_ep, "os_recon_ep");
+	leg->AddEntry(gamma_ss_west_ep, "ss_recon_ep");
+	leg->Draw();
+    }
+    can_gamma->SaveAs("gamma_50to60.eps");
+    can_gamma->SaveAs("gamma_50to60.png");
+
+
 }
